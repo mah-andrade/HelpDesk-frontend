6e9461d987e804ee99ea39eb87865557275994d4
mah-andrade <mahandradesantos.01@gmail.com>
2023-04-08 08:07:45 -0300

    HTML E CSS PARA TECNICO LIST

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Tecnico } from 'src/app/models/tecnico';
 
 @Component({
   selector: 'app-tecnico-list',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TecnicoListComponent implements OnInit {
 
+  ELEMENT_DATA: Tecnico[] = [
+    {
+      id:1,
+      nome:'Matheus Andrade',
+      cpf:'234.234.234-23',
+      email:'mahhard@hotmail.com',
+      senha:'321321',
+      perfis:['0'],
+      dataCriacao:'06/02/2003'
+    }
+  ]
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol','acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
 }
